0f5b2854edea731b905546a0b1dc5c51db0bd687
박정규 <srk99279@naver.com>
2022-06-02 22:03:11 +0900

    Update DS_HW5.c

--- DS_HW_5/DS_HW5.c
+++ DS_HW_5/DS_HW5.c
@@ -5,18 +5,51 @@
 #include <time.h>
 #include <string.h>
 
-#define MAX_COUNT 150000
+#define MAX_COUNT 15000
 #define MAX_LENGTH 20
 
+void insertSort(char(*str)[MAX_LENGTH + 1], int size);
+void bubbleSort(char(*str)[MAX_LENGTH + 1], int size);
 
 
+void insertSort(char (*str)[MAX_LENGTH + 1], int size) {
+	clock_t isStart, isEnd;
+	float isRes;
+	isStart = clock();
 
-void insertSort() {
+	
+	int j;
+	for (int i = 1; i < size; i++) {
+		char tmp[MAX_LENGTH + 1] = { NULL, };
+
+		strcpy(tmp, *(str + i));		
+		for (j = i - 1; j >= 0; j--) {
+			if (strcmp(*(str + j), tmp) > 0)
+				strcpy(*(str + j + 1), *(str + j));
+			else
+				break; 
+		}
+		strcpy(*(str + j + 1), tmp);
+	}
+
+	isEnd = clock();
+
+	isRes = (float)(isEnd - isStart) / CLOCKS_PER_SEC;
+
+	printf("\n");
+
+	/*for (int i = 0; i < size; i++) {
+		printf("%s ", str[i]);
+	}*/
+
+	printf("\nInsert sort : %f", isRes);
+	printf("done");
 
 }
 
-void quickSort() {
-
+void quickSort(char (*str)[MAX_LENGTH+1], int left, int right) {
+	int L = left, R = right;
+	char pivot[MAX_LENGTH + 1] = *(str + ((left + right) / 2));
 }
 
 void mergeSort() {
@@ -28,22 +61,32 @@ void heapSort() {
 }
 
 void bubbleSort(char(*str)[MAX_LENGTH + 1], int size) {
+	clock_t bsStart, bsEnd;
+	float bsRes;
+	bsStart = clock();
+
 	for (int i = 0; i < size - 1; i++)
 	{
 		for (int j = 0; j < size - 1 - i; j++)
 		{
-			
-			if (strcmp(*str, *(str+1)) > 0)
-			{
-				char tmp[MAX_LENGTH] = { NULL, };
-				strcpy(tmp, *str);
-				strcpy(*(str+1), *str);
-				strcpy(*str, tmp);
-			}
-			*(str++);
 
+			if (strcmp(*(str+j), *(str + j + 1)) > 0)
+			{
+				char tmp[MAX_LENGTH+1] = { NULL, };
+				strcpy(tmp, *(str + j));
+				strcpy(*(str + j), *(str + j + 1));
+				strcpy(*(str + j + 1), tmp);
+			}
 		}
 	}
+	bsEnd = clock();
+
+	bsRes = (float)(bsEnd - bsStart) / CLOCKS_PER_SEC;
+
+	printf("\n");
+	
+
+	printf("\nbubble sort : %f", bsRes);
 	printf("done");
 }
 
@@ -54,6 +97,7 @@ int main() {
 	scanf("%d", &n);
 
 	char str[MAX_COUNT][MAX_LENGTH + 1] = { NULL, };
+	
 
 	srand((unsigned int)time(NULL));
 
@@ -63,10 +107,15 @@ int main() {
 		for (int j = 0; j < str_len; j++) {
 			str[i][j] = 'a' + rand() % 26;
 		}
-		printf("%s ", &str[i]);
 	}
 
+	insertSort(str, n);
+	printf("\n");
 
 	bubbleSort(str, n);
+	printf("\n");
+	
 
+	printf("done");
+	return 0;
 }
